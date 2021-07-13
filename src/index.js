e3643c2403bb2f36a881ab047e2cdc6e1cab9111
dima-kulikov <skaizy44@gmail.com>
2021-07-13 23:04:30 +0300

    11
    11

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './sass/main.scss';
+import './sass/_example.scss';
+
+
+class Timer{
+    constructor({element, intialValue = 0,step=1}){
+    this.value = intialValue;
+    this.step = step;
+    }
+
+}
+
+new Timer({
+    element: document.querySelector('#timer-1'),
+    intialValue: 0,
+    step: 1,
+})
