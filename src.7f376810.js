7ece9a4417c6466eebc1ca6d5bdc0e6f0f96e096
dima-kulikov <53553661+dima-kulikov@users.noreply.github.com>
2021-07-13 20:05:05 +0000

    Deploying to gh-pages from @ dima-kulikov/goit-js-hw-10-Webpack@e3643c2403bb2f36a881ab047e2cdc6e1cab9111 🚀

--- src.7f376810.js
+++ src.7f376810.js
@@ -1,6 +1,6 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
-},{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");
-},{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.9139aab2.js.map
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("./sass/main.scss"),require("./sass/_example.scss");class e{constructor({element:e,intialValue:s=0,step:t=1}){this.value=s,this.step=t}}new e({element:document.querySelector("#timer-1"),intialValue:0,step:1});
+},{"./sass/main.scss":"clu1","./sass/_example.scss":"clu1"}]},{},["Focm"], null)
+//# sourceMappingURL=/parcel-project-template/src.7f376810.js.map
