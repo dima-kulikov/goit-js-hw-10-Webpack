3178ee3002d4dc852211d4e804562759a7b96b52
dima-kulikov <skaizy44@gmail.com>
2021-07-18 22:47:52 +0300

    18,07,2021
    18,07,2021

--- src/index.js
+++ src/index.js
@@ -3,15 +3,51 @@ import './sass/_example.scss';
 
 
 class Timer{
-    constructor({element, intialValue = 0,step=1}){
+    constructor({element, intialValue = 0, step=1}){
     this.value = intialValue;
     this.step = step;
+    this.refs={
+    decBtn:document.querySelector('button[data-action="decrement"]'),
+    incBtn:document.querySelector('button[data-action="increment"]'),
+    clockface: element.querySelector('div.value'),
+    };
+    console.log(this.refs);
+    this.updateclockface()
+
+    this.bintEvents();
     }
 
+    increment() {
+        this.value += this.step;
+        this.updateclockface();
+    };
+    decrement() {
+        this.value -= this.step;
+        this.updateclockface();
+    };
+
+updateclockface(){
+    this.refs.clockface.textContent = this.value;
+}    
+
+bintEvents() {
+    this.refs.decBtn.addEventListener('click', this.decrement.bind(this));
+    this.refs.incBtn.addEventListener('click', this.increment.bind(this));
 }
+};
+
+
 
 new Timer({
     element: document.querySelector('#timer-1'),
-    intialValue: 0,
+    intialValue: 10,
+    step: 10,
+
+});
+
+
+new Timer({
+    element: document.querySelector('#timer-2'),
     step: 1,
-})
+});
+//не находит, колько первый 2 раза
